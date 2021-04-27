e6fe412b7663d03e7364cca128bec18dfea6bbfc
AnushaFreshStart <anushagorantla1@gmail.com>
2021-04-27 12:22:18 -0500

    Authenticating requests to Spring Data REST API with HTTPBasic

--- src/main/java/io/jzheaux/springsecurity/resolutions/CurrentUsername.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/CurrentUsername.java
@@ -1,10 +1,13 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@CurrentSecurityContext(expression = "authentication.name")
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface CurrentUsername {
--- src/main/java/io/jzheaux/springsecurity/resolutions/User.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/User.java
@@ -1,6 +1,88 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.UUID;
 
+@Entity(name = "users")
 public class User implements Serializable {
+    @Id
+    UUID id;
+
+    @Column
+    String username;
+
+    @Column
+    String pasword;
+
+    @Column
+    boolean enabled = true;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    Collection<UserAuthority> userAuthorities = new ArrayList<>();
+
+    User() {
+    }
+
+    public User(String username, String password) {
+        this.id = UUID.randomUUID();
+        this.username = username;
+        this.pasword = password;
+    }
+
+    public User(User user) {
+        this.id = user.id;
+        this.username = user.username;
+        this.pasword = user.pasword;
+        this.enabled = user.enabled;
+        this.userAuthorities = user.userAuthorities;
+    }
+
+    public Collection<UserAuthority> getUserAuthorities() {
+        return Collections.unmodifiableCollection(this.userAuthorities);
+    }
+
+    public void setUserAuthorities(Collection<UserAuthority> userAuthorities) {
+        this.userAuthorities = userAuthorities;
+    }
+
+    public void grantAuthority(String authority) {
+        UserAuthority userAuthority = new UserAuthority(this, authority);
+        this.userAuthorities.add(userAuthority);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasword() {
+        return pasword;
+    }
+
+    public void setPasword(String pasword) {
+        this.pasword = pasword;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
